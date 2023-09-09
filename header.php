9e2f17dff599e7ef419cf0be72469ff27ec69f6c
santinobch <santino.bch@gmail.com>
2023-09-09 15:41:19 -0300

    Initial fork of Baskerville

---
+++ header.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+	<head>
+		
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+		
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+						 
+		<?php wp_head(); ?>
+		
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+	
+	</head>
+	
+	<body <?php body_class(); ?>>
+
+		<?php 
+		if ( function_exists( 'wp_body_open' ) ) {
+			wp_body_open(); 
+		}
+		
+		$background_image_url = get_header_image() ? get_header_image() : get_template_directory_uri() . '/assets/images/header.jpg'; 
+		
+		?>
+
+		<a class="skip-link button" href="#site-content"><?php _e( 'Skip to the content', 'baskerville' ); ?></a>
+	
+		<div class="header section small-padding bg-dark bg-image" style="background-image: url( <?php echo esc_url( $background_image_url ); ?> );">
+		
+			<div class="cover"></div>
+			
+			<div class="header-search-block bg-graphite hidden">
+				<?php get_search_form(); ?>
+			</div><!-- .header-search-block -->
+					
+			<div class="header-inner section-inner">
+			
+				<?php 
+
+				$custom_logo_id 	= get_theme_mod( 'custom_logo' );
+				$legacy_logo_url 	= get_theme_mod( 'baskerville_logo' );
+				$blog_title_elem 	= ( ( is_front_page() || is_home() ) && ! is_page() ) ? 'h1' : 'div';
+				$blog_title_class 	= $custom_logo_id ? 'blog-logo' : 'blog-title';
+
+				$blog_title 		= get_bloginfo( 'title' );
+				$blog_description 	= get_bloginfo( 'description' );
+
+				if ( $custom_logo_id  || $legacy_logo_url ) : 
+
+					$custom_logo_url = $custom_logo_id ? wp_get_attachment_image_url( $custom_logo_id, 'full' ) : $legacy_logo_url;
+				
+					?>
+
+					<<?php echo $blog_title_elem; ?> class="<?php echo esc_attr( $blog_title_class ); ?>">
+						<a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<img src="<?php echo esc_url( $custom_logo_url ); ?>">
+							<span class="screen-reader-text"><?php echo $blog_title; ?></span>
+						</a>
+					</<?php echo $blog_title_elem; ?>>
+		
+				<?php elseif ( $blog_description || $blog_title ) : ?>
+
+					<<?php echo $blog_title_elem; ?> class="<?php echo esc_attr( $blog_title_class ); ?>">
+						<a href="<?php echo esc_url( home_url() ); ?>" rel="home"><?php echo $blog_title; ?></a>
+					</<?php echo $blog_title_elem; ?>>
+				
+					<?php if ( $blog_description ) : ?>
+						<h3 class="blog-description"><?php echo $blog_description; ?></h3>
+					<?php endif; ?>
+				
+				<?php endif; ?>
+							
+			</div><!-- .header-inner -->
+						
+		</div><!-- .header -->
+		
+		<div class="navigation section no-padding bg-dark">
+		
+			<div class="navigation-inner section-inner">
+			
+				<button class="nav-toggle toggle fleft hidden">
+					
+					<div class="bar"></div>
+					<div class="bar"></div>
+					<div class="bar"></div>
+					
+				</button>
+						
+				<ul class="main-menu">
+				
+					<?php 
+					
+					if ( has_nav_menu( 'primary' ) ) {
+
+						$nav_args = array( 
+							'container' 		=> '', 
+							'items_wrap' 		=> '%3$s',
+							'theme_location' 	=> 'primary', 
+							'walker' 			=> new baskerville_nav_walker,
+						);
+																		
+						wp_nav_menu( $nav_args ); 
+
+					} else {
+
+						$list_pages_args = array(
+							'container' => '',
+							'title_li' 	=> '',
+						);
+					
+						wp_list_pages( $list_pages_args );
+						
+					} 
+					
+					?>
+											
+				</ul><!-- .main-menu -->
+				 
+				<button class="search-toggle toggle fright">
+					<span class="screen-reader-text"><?php _e( 'Toggle search field', 'baskerville' ); ?></span>
+				</button>
+				 
+				<div class="clear"></div>
+				 
+			</div><!-- .navigation-inner -->
+			
+		</div><!-- .navigation -->
+		
+		<div class="mobile-navigation section bg-graphite no-padding hidden">
+					
+			<ul class="mobile-menu">
+			
+				<?php
+
+				if ( has_nav_menu( 'primary' ) ) {
+																	
+					wp_nav_menu( $nav_args ); 
+
+				} else {
+				
+					wp_list_pages( $list_pages_args );
+					
+				} 
+				
+				?>
+										
+			 </ul><!-- .main-menu -->
+		
+		</div><!-- .mobile-navigation -->
---
+++ template-inicio-santino.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+Template Name: Inicio template
+*/
+
+get_header(); ?>
+
+<style>
+    body {
+        /* Color Bases */
+        --museo-base3: #ffeddeff;
+        --museo-base2: #f9d0adff;
+        --museo-base1: #f3b37bff;
+        --museo-base0: #BE8D62ff;
+        --museo-museo-base00: #896749ff;
+        --museo-base01: #544130ff;
+        --museo-base02: #3a2e24ff;
+        --museo-base03: #1f1a17ff;
+        
+        /* Color Accents */
+        --museo-red: #dc322fff;
+        --museo-orange: #e77817ff;
+        --museo-yellow: #afb500ff;
+        --museo-green: #54b50aff;
+        --museo-aqua: #2aa198ff;
+        --museo-blue: #268bd2ff;
+        --museo-violet: #6c71c4ff;
+        --museo-magenta: #d33682ff;
+    }
+    
+
+    .wrapper-inicio {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .carousel {
+        width: 100vw;
+    }
+    
+    .carousel-control-box {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        height: 60px;
+        width: 60px;
+        border-radius: 30px;
+        
+        background-color: var(--museo-orange);
+    }
+    
+    .carousel-control-prev-icon {
+        margin-right: 6px;
+    }
+    
+    .carousel-control-next-icon {
+        margin-left: 6px;
+    }
+    
+</style>
+
+<div class="wrapper-inicio wrapper section medium-padding " id="site-content" style="padding-top: 0;">
+
+	<div id="carouselExample" class="carousel slide">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+			    <img src="https://museo.ar/wp-content/uploads/2023/04/carousel-1-scaled.jpg" class="d-block w-100" alt="...">
+			    <div class="carousel-caption">
+                    <p class="text-center">
+                        <button class="btn btn-primary">
+                            test
+                        </button>
+                    </p>
+                </div>
+			</div>
+			<div class="carousel-item">
+			    <img src="https://museo.ar/wp-content/uploads/2023/04/carousel-2-scaled.jpg" class="d-block w-100" alt="...">
+			    <div class="carousel-caption">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+		    <div class="carousel-control-box">
+		        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="visually-hidden">Previous</span>
+		    </div>
+			
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+		    <div class="carousel-control-box">
+		        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="visually-hidden">Next</span>
+		    </div>
+		</button>
+	</div>
+										
+	<div class="section-inner">
+	
+	
+	</div><!-- .section-inner -->
+
+</div><!-- .wrapper -->
+								
+<?php get_footer(); ?>
